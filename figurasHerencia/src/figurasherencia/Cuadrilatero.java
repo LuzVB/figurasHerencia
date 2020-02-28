62c30176a4e541563c89b3ada0f98d7fbed50649
Valentina <valentinaing75@gmail.com>
2020-02-27 21:17:01 -0500

    Documentacion
    documentacion de las clases FigurasGeometricas, Triangulo,Cuadrilatero

--- figurasHerencia/src/figurasherencia/Cuadrilatero.java
+++ figurasHerencia/src/figurasherencia/Cuadrilatero.java
@@ -11,10 +11,28 @@ package figurasherencia;
  */
 public class Cuadrilatero extends FigurasGeometricas {
 
-    private double coordenada4X, coordenada4Y;
-    private String coordenada4;
 
-    public Cuadrilatero(double coordenada1X, double coordenada1Y, double coordenada2X, double coordenada2Y, double coordenada3X, double coordenada3Y, double coordenada4X, double coordenada4Y) {
+    /**
+     * las variables coordenada4X y coordenada4Y almacenan los valores de  X y Y de la cuarta coordenada
+     */
+    private double coordenada4X,coordenada4Y;
+    /**
+     * 
+     */
+    private String coordenada4;
+    /**
+     * Constructor que recibe los valores de las cuatro coordendas en X y Y 
+     * @param coordenada1X valor de X de la primera coordenda
+     * @param coordenada1Y valor de Y de la primera coordenda
+     * @param coordenada2X valor de X de la segunda coordenda
+     * @param coordenada2Y valor de Y de la segunda coordenda
+     * @param coordenada3X valor de X de la tercera coordenda
+     * @param coordenada3Y valor de Y de la tercera coordenda
+     * @param coordenada4X valor de X de la cuarta coordenda
+     * @param coordenada4Y valor de Y de la cuarta coordenda
+     */
+    public Cuadrilatero(double coordenada1X, double coordenada1Y, double coordenada2X, double coordenada2Y, double coordenada3X, double coordenada3Y,double coordenada4X, double coordenada4Y ) {
+
         super(coordenada1X, coordenada1Y, coordenada2X, coordenada2Y, coordenada3X, coordenada3Y);
         this.coordenada4X = coordenada4X;
         this.coordenada4Y = coordenada4Y;
@@ -43,6 +61,20 @@ public class Cuadrilatero extends FigurasGeometricas {
         } else {
             return verificar = false;
         }
+    }
+    /**
+     * Metodo que verifica si la figura es un cuadrado un rectangulo
+     * @return verificar retorna true si la figura es un cuadrado ó un fal si es un  rectangulo
+     */
+    public boolean verificarCuadrilatero(){
+        boolean verificar=true;
+        if(getLado1() == getLado2()){
+            return verificar=true;
+        }
+        else{
+            return verificar=false;
+
+        }
 
 //        if(verificarCoord == true){
 //            //si son iguales es un Cuadrado
@@ -63,7 +95,12 @@ public class Cuadrilatero extends FigurasGeometricas {
 //        }
     }
 
-    //verificar coordenadas para No dibujar un ROMBO o un RomBoide
+    
+    /**
+     * Metodo para verificar  que no se dibuje un rombo ó un romboide
+     * @return verificar devuelve un true si la condicion se cumple
+     */
+
     @Override
     public boolean verificarCoordenadas() {
         boolean verificar = false;
@@ -94,26 +131,47 @@ public class Cuadrilatero extends FigurasGeometricas {
 //        // hacer logica segun el trinagulo , reescribir en cuadrilatero 
 //        return puntosEnY;
 //    }
+
+    /**
+     * Metodo para mostrar el valor de X  de la cuarta coordenda
+     * @return coordenada4X retorna el valor de X de la cuarta coordenda
+     */
+
     public double getCoordenada4X() {
         return coordenada4X;
     }
-
+    /**
+     * Metodo para modificar el valor de X de la cuarta coordenda  
+     * @param coordenada4X valor a modificar en X  de la cuarta coordenda
+     */
     public void setCoordenada4X(short coordenada4X) {
         this.coordenada4X = coordenada4X;
     }
-
+    /**
+     * Metodo para mostrar el valor de Y  de la cuarta coordenda
+     * @return retorna el valor de Y de la cuarta coordenda
+     */
     public double getCoordenada4Y() {
         return coordenada4Y;
     }
-
+    /**
+     * Metodo para modificar el valor de Y de la cuarta coordenda  
+     * @param coordenada4Y valor a modificar en Y  de la cuarta coordenda
+     */
     public void setCoordenada4Y(short coordenada4Y) {
         this.coordenada4Y = coordenada4Y;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public String getCoordenada4() {
         return coordenada4;
     }
-
+    /**
+     * 
+     * @param coordenada4 
+     */
     public void setCoordenada4(String coordenada4) {
         this.coordenada4 = coordenada4;
     }
--- figurasHerencia/src/figurasherencia/FigurasGeometricas.java
+++ figurasHerencia/src/figurasherencia/FigurasGeometricas.java
@@ -5,30 +5,55 @@
  */
 package figurasherencia;
 
-
 /**
- *
+ * Clase padre 
  * @author Valentina Sarmiento
  */
 public class FigurasGeometricas {
-
+    /**
+     * la variable lado1 almacena el valor del primer lado de la figura
+     */
     private double lado1;
+    /**
+     * la variable lado2 almacena el valor del segundo lado de la figura
+     */
     private double lado2;
+    /**
+     *la variable area almacena el valor del area la figura
+     */
     private double area;
+    /**
+     *la variable perimetro almacena el valor del  periemetro de la figura
+     */
     private double perimetro;
+    /**
+     * las variables coordenada1X y coordenada1Y  almacenan los valores de  X y Y de la primera coordenada 
+     */
     private double coordenada1X, coordenada1Y;
+    /**
+     * las variables coordenada2X y coordenada2Y   almacenan los valores de  X y Y de la segunda coordenada
+     */
     private double coordenada2X, coordenada2Y;
+    /**
+     * las variables coordenada3X y coordenada3Y   almacenan los valores de  X y Y de la tercera coordenada
+     */
     private double coordenada3X, coordenada3Y;
     private String coordenada1;
     private String coordenada2;
     private String coordenada3;
     private String color;
-
     //Quitar decimales alas variables doubles
     //DecimalFormat decimales = new DecimalFormat("#.0");
-
-
     //pensar si hacer otro constructor para los lados
+    /**
+     * Constructor que recibe los valores de las tres coordendas en X y Y  
+     * @param coordenada1X valor de X de la primera coordenda 
+     * @param coordenada1Y valor de Y de la primera coordenda 
+     * @param coordenada2X valor de X de la segunda coordenda 
+     * @param coordenada2Y valor de Y de la segunda coordenda 
+     * @param coordenada3X valor de X de la tercera coordenda 
+     * @param coordenada3Y valor de Y de la tercera coordenda 
+     */
     public FigurasGeometricas(double coordenada1X, double coordenada1Y, double coordenada2X, double coordenada2Y, double coordenada3X, double coordenada3Y) {
         this.coordenada1X = coordenada1X;
         this.coordenada1Y = coordenada1Y;
@@ -36,72 +61,102 @@ public class FigurasGeometricas {
         this.coordenada2Y = coordenada2Y;
         this.coordenada3X = coordenada3X;
         this.coordenada3Y = coordenada3Y;
+
         
 //        System.out.print("1/"+coordenada1X+","+coordenada1Y+"\n");
 //        System.out.print("2/"+coordenada2X+","+coordenada2Y+"\n");
 //        System.out.print("3/"+coordenada3X+","+coordenada3Y+"\n");
         
-        
+     
+
+        System.out.print("1/" + coordenada1X + "," + coordenada1Y + "\n");
+        System.out.print("2/" + coordenada2X + "," + coordenada2Y + "\n");
+        System.out.print("3/" + coordenada3X + "," + coordenada3Y + "\n");
+
         hallarLado();
     }
 
     /*public FigurasGeometricas(int lado1) {
-        this.lado1 = lado1;
-    }*/
+     this.lado1 = lado1;
+     }*/
+    /**
+     * Metodo que recibe las coordenadas en pares , para validar si se puede
+     * dibujar un triangulo
+     * @return verificar retorna un false si se cumplen las condiciones 
+     */
+    public boolean verificarCoordenadas() {
+        boolean verificar = true;
 
-    //Recibe las coordenadas en pares , para validar si se puede hacer un triangulo en caso de que si se envia un true
-    public boolean verificarCoordenadas(){
-        boolean verificar=true;
-        
-        if(this.coordenada1X==this.coordenada2X && this.coordenada2Y == this.coordenada3Y  ){   
+        if (this.coordenada1X == this.coordenada2X && this.coordenada2Y == this.coordenada3Y) {
             verificar = false;
         }
         return verificar;
     }
 
-    //saca la distancia de un lado 
+    /**
+     * Metodo para hallar la distancia de cada uno de los lados
+     */
     public void hallarLado() {
-        double raiz,raiz2;
+        double raiz, raiz2;
         double potencia1, potencia2;
 
         potencia1 = Math.pow((this.coordenada2X - this.coordenada1X), 2);
         potencia2 = Math.pow((this.coordenada2Y - this.coordenada1Y), 2);
-        raiz = Math.sqrt(potencia1+potencia2);
-        
-        this.lado1 = raiz ;
-        
+        raiz = Math.sqrt(potencia1 + potencia2);
+
+        this.lado1 = raiz;
+
         potencia1 = Math.pow((this.coordenada3X - this.coordenada2X), 2);
         potencia2 = Math.pow((this.coordenada3Y - this.coordenada2Y), 2);
-        raiz = Math.sqrt(potencia1+potencia2);
-        
-        this.lado2 = raiz ;
+        raiz = Math.sqrt(potencia1 + potencia2);
+
+        this.lado2 = raiz;
     }
 
-    //Genera los arrays de puntos x y y para graficar un poligono
+    /**
+     * Metodo que genera los arrays de los puntos x para graficar un poligono
+     * @return puntoEnX retorna un array
+     */
     public int[] arrayPuntosEnX() {
        //int puntosEnX[] = {coordenada1X, coordenada2X, coordenada3X};
-        int puntosEnX[]= {1, 2, 3};
+
+        int coordenadaX1 = (int) this.coordenada1X;
+        int coordenadaX2 = (int) this.coordenada2X;
+        int coordenadaX3 = (int) this.coordenada3X;
+
+        //int puntosEnX[] = {coordenadaX1, coordenadaX2, coordenadaX3};
+        int puntosEnX[] = {1, 2, 3};
         // hacer logica segun el trinagulo , reescribir en cuadrilatero 
         return puntosEnX;
     }
 
+    /**
+     * Metodo que genera los arrays de los puntos y para graficar un poligono
+     * @return puntoEnY retorna un array
+     */
     public int[] arrayPuntosEnY() {
-        
+
         int coordenadaY1 = (int) this.coordenada1Y;
         int coordenadaY2 = (int) this.coordenada2Y;
         int coordenadaY3 = (int) this.coordenada3Y;
 //        int puntosEnY[] = {coordenadaY1, coordenadaY2, coordenadaY3};
 //        // hacer logica segun el trinagulo , reescribir en cuadrilatero 
 //        return puntosEnY;
-         int puntosEnX[]= {1, 2, 3};
+        int puntosEnX[] = {1, 2, 3};
         // hacer logica segun el trinagulo , reescribir en cuadrilatero 
         return puntosEnX;
     }
 
+    /**
+     * Metodo para hallar el area de un cuadrado
+     */
     public void hallarArea() {
         this.area = lado1 * lado1;
     }
 
+    /**
+     * Metodo para hallar el perimetro de un cuadrado
+     */
     public void hallarPerimetro() {
         this.perimetro = lado1 * 4;
     }
@@ -111,91 +166,170 @@ public class FigurasGeometricas {
         
         return resultado;
     }
+    /**
+     * Metodo para pintar la figura geometrica
+     */
+    public void pintar() {
 
+
+    }
+
+    /**
+     * Metodo para mostrar el valor del primer lado 
+     * @return lado1 retorna el valor del primer lado
+     */
     public double getLado1() {
         return lado1;
     }
 
+    /**
+     * Metodo para modificar el valor del primer lado
+     * @param lado1 valor a modificar del primer lado 
+     */
     public void setLado1(double lado1) {
         this.lado1 = lado1;
     }
 
+    /**
+     * Metodo para mostrar el valor del segundo lado
+     * @return lado2 retorna el valor del segundo lado
+     */
     public double getLado2() {
         return lado2;
     }
 
     /**
-     *
+     * Metodo para modificar el valor del segundo lado
+     * @param lado2 valor a modificar del segundo lado
      */
+
     public void setLado2(double lado2) {
         this.lado2 = lado2;
     }
-   
-
+    
+    /**
+     * Metodo para mostrar el valor del area
+     * @return area retorna el valor del area 
+     */
     public double getArea() {
         return area;
     }
 
+    /**
+     * Metodo para modificar el valor del area
+     * @param area valor a modificar del area
+     */
     public void setArea(double area) {
         this.area = area;
     }
-
+    /**
+     * Metodo para mostrar el valor del perimetro
+     * @return perimetro retorna el valor del perimetro
+     */
     public double getPerimetro() {
         return perimetro;
     }
-
+    /**
+     * Metodo para modificar el valor del perimetro
+     * @param perimetro valor a modificar del perimetro
+     */
     public void setPerimetro(double perimetro) {
         this.perimetro = perimetro;
     }
-
+    /**
+     * Metodo para mostrar el valor de X  de la primera coordenada
+     * @return coordenada1X retorna el valor de X de la primera coordenda
+     */
+    
     public double getCoordenada1X() {
         return coordenada1X;
     }
-
+    /**
+     * Metodo para modificar el valor de X de la primera coordenda 
+     * @param coordenada1X valor a modificar en X de la primera coordenda 
+     */
     public void setCoordenada1X(double coordenada1X) {
         this.coordenada1X = coordenada1X;
     }
-
+    /**
+     * Metodo para mostrar el valor de Y  de la primera coordenada
+     * @return coordenada1Y retorna el valor de Y de la primera coordenda
+     */
     public double getCoordenada1Y() {
         return coordenada1Y;
     }
-
+    /**
+     * Metodo para modificar el valor de Y de la primera coordenda 
+     * @param coordenada1Y valor a modificar en Y  de la primera coordenda
+     */
     public void setCoordenada1Y(double coordenada1Y) {
         this.coordenada1Y = coordenada1Y;
     }
-
+    /**
+     * Metodo para mostrar el valor de X  de la segunda coordenda
+     * @return coordenada2X retorna el valor de X de la segunda coordenda
+     */
     public double getCoordenada2X() {
         return coordenada2X;
     }
-
+    /**
+     * Metodo para modificar el valor de X de la segunda coordenda 
+     * @param coordenada2X valor a modificar en X  de la segunda coordenda
+     */
     public void setCoordenada2X(double coordenada2X) {
         this.coordenada2X = coordenada2X;
     }
-
+    /**
+     * Metodo para mostrar el valor de Y  de la segunda coordenda
+     * @return coordenada2Y  retorna el valor de Y de la segunda coordenda
+     */
     public double getCoordenada2Y() {
         return coordenada2Y;
     }
-
+    
+    /**
+     * Metodo para modificar el valor de Y de la segunda coordenda  
+     * @param coordenada2Y valor a modificar en Y  de la segunda coordenda
+     */
     public void setCoordenada2Y(double coordenada2Y) {
         this.coordenada2Y = coordenada2Y;
     }
-
+    
+    /**
+     * Metodo para mostrar el valor de X  de la tercera coordenda
+     * @return coordenada3X retorna el valor de X de la tercera coordenda
+     */
     public double getCoordenada3X() {
-        return coordenada3X;
+        return coordenada3X; 
     }
-
+    
+    /**
+     * Metodo para modificar el valor de X de la tercera coordenda  
+     * @param coordenada3X valor a modificar en X  de la tercera coordenda
+     */
     public void setCoordenada3X(double coordenada3X) {
         this.coordenada3X = coordenada3X;
     }
+    /**
+     * Metodo para mostrar el valor de Y  de la tercera coordenda
+     * @return coordenada3Y retorna el valor de Y de la tercera coordenda
+     */
 
     public double getCoordenada3Y() {
         return coordenada3Y;
     }
+    /**
+     * Metodo para modificar el valor de X de la tercera coordenda  
+     * @param coordenada3Y valor a modificar en Y  de la tercera coordenda
+     */
 
     public void setCoordenada3Y(double coordenada3Y) {
         this.coordenada3Y = coordenada3Y;
     }
-
+    /**
+     * Metodo para mostrar el valor de la segunda cordenada en X
+     * @return 
+     */
     public String getCoordenada1() {
         return coordenada1;
     }
